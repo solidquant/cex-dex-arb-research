f0bc1ca9fb928c79f62cc651f644e838c8608025
Hadleymci <105074432+Hadleymci@users.noreply.github.com>
2023-08-10 15:13:48 -0400

    Uni V2/3 + Curve predictooorrr.py

--- Uni v2:v3 price predict.py
+++ Uni v2:v3 price predict.py
@@ -1,59 +1,58 @@
+**************
+Needs Curve.fi correct pools. Not sure which is correct as I am unfamiliar with Curve. 
+A good place to start: https://curve.readthedocs.io/ref-addresses.html
+******************
+
 from web3 import Web3
 
 # Connect to your local Ethereum node
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))  # Change if using different port or IPC
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
-# Uniswap V2 Pair ABI
-UNISWAP_V2_PAIR_ABI = [
-    {
-        "constant": True,
-        "inputs": [],
-        "name": "getReserves",
-        "outputs": [
-            {"internalType": "uint112", "name": "_reserve0", "type": "uint112"},
-            {"internalType": "uint112", "name": "_reserve1", "type": "uint112"},
-            {"internalType": "uint32", "name": "_blockTimestampLast", "type": "uint32"}
-        ],
-        "type": "function"
-    }
-]
+# Uniswap V2 Pair ABI, Uniswap V3 Pool ABI
+# ... [As previously defined] ...
 
-# Uniswap V3 Pool ABI
-UNISWAP_V3_POOL_ABI = [
-    {
-        "constant": True,
-        "inputs": [],
-        "name": "slot0",
-        "outputs": [
-            {"internalType": "uint160", "name": "sqrtPriceX96", "type": "uint160"},
-            {"internalType": "int24", "name": "tick", "type": "int24"},
-            # ... other outputs ...
-        ],
-        "type": "function"
-    }
+# Placeholder for Curve pool ABI
+CURVE_POOL_ABI = [
+    # ... [Placeholder ABI for Curve Finance; fill when needed] ...
 ]
 
 def get_uniswap_v2_price(pair_address):
-    contract = w3.eth.contract(address=pair_address, abi=UNISWAP_V2_PAIR_ABI)
-    reserve0, reserve1, _ = contract.functions.getReserves().call()
-    return reserve1 / reserve0
+    # ... [As previously defined] ...
 
 def get_uniswap_v3_price(pool_address):
-    contract = w3.eth.contract(address=pool_address, abi=UNISWAP_V3_POOL_ABI)
-    slot_data = contract.functions.slot0().call()
-    sqrt_price_x96 = slot_data[0]
-    price = (sqrt_price_x96 ** 2) / (1 << 192)
-    return price
+    # ... [As previously defined] ...
+
+def get_curve_price(pool_address, token_in_idx, token_out_idx):
+    """Simulate a 1 WETH trade to get the USDC amount using Curve."""
+    contract = w3.eth.contract(address=pool_address, abi=CURVE_POOL_ABI)
+    
+    # Simulate trading 1 WETH
+    amount_in = 1e18
+    dy = contract.functions.get_dy(token_in_idx, token_out_idx, amount_in).call()
+    
+    return dy / amount_in
 
 def main():
-    v2_pair_address = "0x...yourUniswapV2PairAddress..."
-    v3_pool_address = "0x...yourUniswapV3PoolAddress..."
+    # WETH and USDC contract addresses
+    WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    USDC_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+    
+    # Uniswap V2 and V3 contract addresses
+    v2_pair_address = "0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc"
+    v3_pool_address = "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640"
+    
+    # Placeholder for Curve pool address and token indices
+    curve_pool_address = "0x...yourCurvePoolAddressForWETH_USDC..."
+    weth_idx = ...  # Placeholder for WETH index in the Curve pool
+    usdc_idx = ...  # Placeholder for USDC index in the Curve pool
 
     v2_price = get_uniswap_v2_price(v2_pair_address)
     v3_price = get_uniswap_v3_price(v3_pool_address)
+    curve_price = get_curve_price(curve_pool_address, weth_idx, usdc_idx)
 
-    print(f"Uniswap V2 Price: {v2_price} TOKEN/ETH")
-    print(f"Uniswap V3 Price: {v3_price} TOKEN1/ETH")
+    print(f"Uniswap V2 Price for 1 WETH: {v2_price} USDC")
+    print(f"Uniswap V3 Price for 1 WETH: {v3_price} USDC")
+    print(f"Curve Finance Price for 1 WETH: {curve_price} USDC")
 
 if __name__ == "__main__":
     main()
